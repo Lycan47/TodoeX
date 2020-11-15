11b601ad9049d43a565ce6a1f32dbc2973d36ccd
Lycan47 <hamzashaikh74@gmail.com>
2020-11-15 23:44:13 +0530

    15/11/2020 - Input Box & Add Button

--- todoex/src/App.js
+++ todoex/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="app">
+      <header className="app_header">
         <h1>TodoeX</h1>
-        <p>
+        <p className = "beyonce"> 
           A Todo App
         </p>
+        <textarea type = "text" className='input_field' name = 'Todo list' placeholder= "e.g. Get pastries sun at 9 #AbhaySingh"/>
+        <input type="submit" className='add_button' value="Add a task" />
+        
+        
       </header>
     </div>
   );
