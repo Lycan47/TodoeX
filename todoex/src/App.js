6a1c20902187585fb804c1cd91bd807bdaff197c
Lycan47 <hamzashaikh74@gmail.com>
2020-11-17 00:11:44 +0530

    Added Date Time Picker

--- todoex/src/App.js
+++ todoex/src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DateFnsUtils from '@date-io/date-fns';
 import './App.css';
+import {
+  DateTimePicker,
+  MuiPickersUtilsProvider,
+} from '@material-ui/pickers';
+import { Button } from '@material-ui/core';
+// https://material-ui-pickers.dev/guides/css-overrides
 
 
 function App() {
+  const [selectedDate, handleDateChange] = useState(new Date());
+
   return (
     <div className="app">
       <header className="app_header">
@@ -10,13 +19,22 @@ function App() {
         <p className = "beyonce"> 
           A Todo App
         </p>
-        <textarea type = "text" className='input_field' name = 'Todo list' placeholder= "e.g. Get pastries sun at 9 #AbhaySingh"/>
+        <textarea type = "text" className='input_field' name = 'Todo list' placeholder= "e.g. Get pastries sun at 9 #Pastries"/>
         <input type="submit" className='add_button' value="Add a task" />
+        <div className='date_time'>
+          
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <DateTimePicker value={selectedDate} onChange={handleDateChange} />
+          </MuiPickersUtilsProvider>
         
-        
+
+        </div>
       </header>
     </div>
   );
 }
+
+
+
 
 export default App;
